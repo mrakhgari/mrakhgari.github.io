4976fb55ce3539a58997eec92875e5e81bf9d83b
muhammadreza <mra.akhgari@gmail.com>
2022-12-14 10:14:27 +0330

    Build

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import ContactView from '../views/ContactView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/me/'),
   routes: [
     {
       path: '/about',
